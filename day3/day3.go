84d66b32b7a5b66df63edf4bd7493145daf6dc89
Marcus <marcusicaromc@gmail.com>
2024-12-03 22:38:46 -0300

    day 3 separate pt 1 from pt 2

--- day3/day3.go
+++ day3/day3.go
@@ -17,7 +17,7 @@ func main() {
 
 	content := strings.Join(filecontent, "\n")
 
-	pattern := `mul\(\d{1,3},\d{1,3\)|do\(\)|don't\(\)`
+	pattern := `mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)`
 	re := regexp.MustCompile(pattern)
 
 	matches := re.FindAllString(content, -1)
@@ -25,7 +25,29 @@ func main() {
 	patternNums := `\d+`
 	reNums := regexp.MustCompile(patternNums)
 
-	result := 0
+	resultPt1 := 0
+
+	for _, match := range matches {
+		if match == "do()" {
+			continue
+		} else if match == "don't()" {
+			continue
+		}
+
+		matchesNums := reNums.FindAllString(match, -1)
+		num1, err1 := strconv.Atoi(matchesNums[0])
+		num2, err2 := strconv.Atoi(matchesNums[1])
+
+		if err1 != nil || err2 != nil {
+			fmt.Println("Error converting string to int")
+			return
+		}
+
+		resultPt1 += num1 * num2
+
+	}
+
+	resultPt2 := 0
 	consider := true
 
 	for _, match := range matches {
@@ -48,8 +70,13 @@ func main() {
 			return
 		}
 
-		result += num1 * num2
+		resultPt2 += num1 * num2
 	}
+	fmt.Println("pt1: ")
 
-	fmt.Println(result)
+	fmt.Println(resultPt1)
+
+	fmt.Println("pt2: ")
+
+	fmt.Println(resultPt2)
 }
